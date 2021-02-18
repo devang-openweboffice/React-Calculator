c133473215559e797c204f3ef162dbfbad4c80ab
Devang Pujara <pujaradevang@gmail.com>
2021-02-18 14:13:27 +0530

    initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "./components/Header";
+import Calculator from "./components/Calculator";
+
+function App() {
+    return (
+        <div className="app_wrapper">
+            <Header title="Calculator" />
+            <Calculator />
+        </div>
+    );
+}
+
+export default App;
